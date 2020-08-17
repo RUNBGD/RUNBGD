b61cb78df1b73ef75afe81e66b7156560e170ee7
Milos Mladenovic <milos.mladenovic.work@gmail.com>
2020-08-17 19:26:10 +0200

    added advertise page

---
+++ src/pages/advertise/index.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+import Layout from '../../components/Layout'
+import styles from './advertise.module.scss'
+import logoImg from '../../img/logo.jpeg'
+
+const AdvertisePage = () => {
+  return(
+    <Layout>
+      <main>
+        <div className={styles.advertiseCard}>
+          <div className={styles.lRow}>
+            <h1 className={styles.title}>ADVERTISE WITH US</h1>
+            <img className={styles.logo} src={logoImg} alt=''/>
+          </div>
+          <p>
+            The world is changing quickly, and connecting with a younger audience is becoming more challenging than ever. We're here to share our learnings from working with some of the top marketers in the world in how we've helped them future-proof their business.
+          </p>
+          <a className={styles.actionButton} href='mailto:advertise@runbgd.com'>
+            GET IN TOUCH
+          </a>
+        </div>
+      </main>
+    </Layout>
+  )
+}
+
+export default AdvertisePage
