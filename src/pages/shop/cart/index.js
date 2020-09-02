2f63533cbdc2e4fb7afedd3d471f0a92302d87f9
Milos Mladenovic <milos.mladenovic.work@gmail.com>
2020-09-02 18:26:03 +0200

    added descriptions, quantity and item names in payments

--- src/pages/shop/cart/index.js
+++ src/pages/shop/cart/index.js
@@ -59,7 +59,39 @@ const Cart = () => {
                     </p>
                     <div className={styles.paymentMethodsContainer}>
                         <PayPalButton
-                            amount='10'
+                            amount={products && products.reduce((a, b) => {console.log(a);return (a  + b.quantity * b.product.frontmatter.price)}, 0)}
+                            createOrder={(data, actions) => {
+                                return actions.order.create({
+                                    purchase_units: [{
+                                        amount:{
+                                            value:products && products.reduce((a, b) => {console.log(a);return (a  + b.quantity * b.product.frontmatter.price)}, 0),
+                                            currency_code: "EUR",
+                                            breakdown:{
+                                                item_total:{
+                                                    currency_code: "EUR",
+                                                    value: products && products.reduce((a, b) => {console.log(a);return (a  + b.quantity * b.product.frontmatter.price)}, 0)
+                                                }
+                                            }
+                                        },
+                                        items:[
+                                            ...products.map(product => {
+                                                return {
+                                                    name:product.product.frontmatter.title,
+                                                    quantity: product.quantity,
+                                                    description:`${product.size != undefined ? ('Size:' + product.product.frontmatter.sizes[product.size].size) : ''}`,
+                                                    sku:product.product.fields.slug,
+                                                    unit_amount:{
+                                                        currency_code: "EUR",
+                                                        value: product.product.frontmatter.price
+                                                    }
+                                                }
+                                            })
+                                        ]
+                                  },
+                                
+                                ],
+                                });
+                              }}
                             onSuccess={
                                 (details, data) => {
                                     alert("Transaction completed by " + details.payer.name.given_name)
@@ -67,11 +99,9 @@ const Cart = () => {
                             }
                             options={{
                                 currency:"EUR",
-                                clientId: "AboCmFGTRBEiShVijJzFLGiSo5P0L-hZcD0BKbNqxalY6APNo10R_td8yWa2arHA0xSeIQOAz7kS-eKQ"
+                                clientId: process.env.GATSBY_PAYPAL_CLIENT_ID
                               }}
                         />
-                        <button>PayPal</button>
-                        <button>Credit Card</button>
                     </div>
                 </div>
             </div>
