48b41f9d394c1f9c201d78a235e8d1babc64acd5
Milos Mladenovic <milos.mladenovic.work@gmail.com>
2020-09-21 20:03:07 +0200

    added different map layer

--- src/components/FindPlacesMap/index.js
+++ src/components/FindPlacesMap/index.js
@@ -54,8 +54,9 @@ const FindPlacesMap = ({locations, zoom, expanded, xCoord, yCoord, currentX, cur
             typeof window !== 'undefined' &&
             <Map center={currentY == undefined ? [44.823070, 20.453420] : [currentY, currentX]} zoom={zoom}>
                 <TileLayer
-                url="https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png"
-                attribution='&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
+                url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
+                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
+                subdomains={'abcd'}
                 />
                 {locations.map(({node:location}) => {
                     
