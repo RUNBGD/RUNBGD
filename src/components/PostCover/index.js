0f1a6f37d12c0890a43e68e8772d442af6707842
Milos Mladenovic <milos.mladenovic.work@gmail.com>
2020-11-23 19:15:18 +0100

    added different public folder for images

--- src/components/PostCover/index.js
+++ src/components/PostCover/index.js
@@ -59,13 +59,18 @@ const PostCover = ({ post }) => {
     authorSlug = author.node.fields.slug
   }
 
+  console.log(post.frontmatter)
+
   return (
     <div className={styles.post}>
       <div className={styles.postImage}>
-        <PostImage
-          slug={post.fields.slug}
-          image={post.frontmatter.coverImage.childImageSharp.fluid}
-        />
+        {
+          // post.frontmatter.coverImage &&
+          <PostImage
+            slug={post.fields.slug}
+            image={post.frontmatter.coverImage.childImageSharp.fluid}
+          />
+        }
       </div>
       <div className={styles.postText}>
         <div className={styles.postDetails}>
