33e239df8d0ffaa2cfd8c50f868ba29ccdd8c660
Milos Mladenovic <milos.mladenovic.work@gmail.com>
2020-12-04 00:19:31 +0100

    added shorter category slugs

--- gatsby-node.js
+++ gatsby-node.js
@@ -440,7 +440,7 @@ exports.createPages = async ({ actions, graphql }) => {
       const title = edge.node.frontmatter.title
 
       createPage({
-        path: edge.node.fields.slug,
+        path: title,
         component: path.resolve(
           `src/templates/${String(edge.node.frontmatter.templateKey)}/index.js`
         ),
