87d0e3f0b34d3b6614eda6d3370ffd5d8f886874
Milos Mladenovic <milos.mladenovic.work@gmail.com>
2020-12-08 18:46:18 +0100

    made order mail for pay on arrival method

--- functions/pay-on-arrival-form.js
+++ functions/pay-on-arrival-form.js
@@ -1,33 +1,35 @@
-require('dotenv').config()
-const sgMail = require('@sendgrid/mail')
+"use strict";
 
-const {SENDGRID_API_KEY, SENDGRID_TO_EMAIL} = process.env
+require('dotenv').config();
 
-sgMail.setApiKey(SENDGRID_API_KEY)
+const sgMail = require('@sendgrid/mail');
 
+const {
+  SENDGRID_API_KEY,
+  SENDGRID_TO_EMAIL
+} = process.env;
+sgMail.setApiKey(SENDGRID_API_KEY);
 
-
-exports.handler = async function(event, context) {
-    const {
-        email,
-        firstName,
-        lastName,
-        address,
-        phoneNumber,
-        country,
-        city,
-        zipCode,
-        message,
-        price,
-        products,
-        state
-    } = event.queryStringParameters
-    
-    const msgToSeller = {
-        to: SENDGRID_TO_EMAIL,
-        from: SENDGRID_TO_EMAIL,
-        subject: 'Order',
-        html:`
+exports.handler = async function (event, context) {
+  const {
+    email,
+    firstName,
+    lastName,
+    address,
+    phoneNumber,
+    country,
+    city,
+    zipCode,
+    message,
+    price,
+    products,
+    state
+  } = event.queryStringParameters;
+  const msgToSeller = {
+    to: SENDGRID_TO_EMAIL,
+    from: SENDGRID_TO_EMAIL,
+    subject: 'Order',
+    html: `
             <table style='margin-bottom:15px; border:1px solid #000000;border-collapse: collapse'>
             <tr>
                 <th style='padding:2.5px; border:1px solid #000000; text-align:center;background: #626262; color: white'>First Name</th>
@@ -75,25 +77,25 @@ exports.handler = async function(event, context) {
                    <th style='padding:2.5px; border:1px solid #000000; text-align:center;background: #626262; color: white'>UID</th> 
                 </tr>
                 ${JSON.parse(products).map(product => {
-                    return `<tr>
+      return `<tr>
                         <td style='padding:2.5px; border:1px solid #000000; text-align:center;'>${product.quantity}</td>
                         <td style='padding:2.5px; border:1px solid #000000; text-align:center;'>${product.title}</td>
+                        <td style='padding:2.5px; border:1px solid #000000; text-align:center;'>${product.color}</td>
                         <td style='padding:2.5px; border:1px solid #000000; text-align:center;'>${product.size}</td>
                         <td style='padding:2.5px; border:1px solid #000000; text-align:center;'>${product.uid}</td>
-                    </tr>`
-                })}
+                    </tr>`;
+    })}
                 <tr style='width:100%'><th colspan='5' style='padding:2.5px; border:1px solid #000000; text-align:center;background: #626262; color: white'>Total</th></tr>                
                 <tr style='width:100%'><td colspan='5' style='padding:2.5px; border:1px solid #000000; text-align:center;'>€${price}</td></tr>                
             </table>
             <p>Message: ${message}</p>
         `
-      }
-
-      const msgToBuyer = {
-        to: email,
-        from: SENDGRID_TO_EMAIL,
-        subject: 'Order from RUNBGD',
-        html:`
+  };
+  const msgToBuyer = {
+    to: email,
+    from: SENDGRID_TO_EMAIL,
+    subject: 'Order from Minus1 Shop',
+    html: `
             <table style='margin-bottom:15px; border:1px solid #000000;border-collapse: collapse'>
             <tr>
                 <th style='padding:2.5px; border:1px solid #000000; text-align:center;background: #626262; color: white'>First Name</th>
@@ -141,39 +143,40 @@ exports.handler = async function(event, context) {
                    <th style='padding:2.5px; border:1px solid #000000; text-align:center;background: #626262; color: white'>UID</th> 
                 </tr>
                 ${JSON.parse(products).map(product => {
-                    return `<tr>
+      return `<tr>
                         <td style='padding:2.5px; border:1px solid #000000; text-align:center;'>${product.quantity}</td>
                         <td style='padding:2.5px; border:1px solid #000000; text-align:center;'>${product.title}</td>
+                        <td style='padding:2.5px; border:1px solid #000000; text-align:center;'>${product.color}</td>
                         <td style='padding:2.5px; border:1px solid #000000; text-align:center;'>${product.size}</td>
                         <td style='padding:2.5px; border:1px solid #000000; text-align:center;'>${product.uid}</td>
-                    </tr>`
-                })}
+                    </tr>`;
+    })}
                 <tr style='width:100%'><th colspan='5' style='padding:2.5px; border:1px solid #000000; text-align:center;background: #626262; color: white'>Total</th></tr>                
                 <tr style='width:100%'><td colspan='5' style='padding:2.5px; border:1px solid #000000; text-align:center;'>€${price}</td></tr>                
             </table>
             <p>Please respond to this email if you have any questions regarding your order, thanks!</p>
         `
-      }
+  };
 
-    try{
-          await sgMail.send(msgToSeller);
-          await sgMail.send(msgToBuyer);
-          let successBody = JSON.stringify({
-              status:'success',
-              message:'Order successfuly placed, wait for email for details!'
-          })
-          return {
-              statusCode:200,
-              body:successBody
-            }
-        }catch(error){
-            let errorBody = JSON.stringify({
-              status:'error',
-              message:'There was some error with our servers. Try later!'
-            })          
-            return {
-            statusCode:500,
-            body: errorBody
-        }
-    }
+  try {
+    await sgMail.send(msgToSeller);
+    await sgMail.send(msgToBuyer);
+    let successBody = JSON.stringify({
+      status: 'success',
+      message: 'Order successfuly placed, wait for email for details!'
+    });
+    return {
+      statusCode: 200,
+      body: successBody
+    };
+  } catch (error) {
+    let errorBody = JSON.stringify({
+      status: 'error',
+      message: 'There was some error with our servers. Try later!'
+    });
+    return {
+      statusCode: 500,
+      body: errorBody
+    };
   }
+};
