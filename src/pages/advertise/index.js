845877690e2952bb13d0550ac34c7714d591d323
Milos Mladenovic <milos.mladenovic.work@gmail.com>
2020-12-18 19:10:22 +0100

    added seo for advertise page

--- src/pages/advertise/index.js
+++ src/pages/advertise/index.js
@@ -1,20 +1,45 @@
 import React from 'react'
 import { Helmet } from 'react-helmet'
+import { useStaticQuery, graphql} from 'gatsby'
 
 import Layout from '../../components/Layout'
 import styles from './advertise.module.scss'
 import logoImg from '../../img/logo.jpeg'
 
 const AdvertisePage = () => {
+
+  const data = useStaticQuery(graphql`
+    query AdvertisePageQuery {
+      page:markdownRemark(frontmatter:{templateKey:{eq:"advertise-page"}}) {
+        frontmatter {
+          seo
+          seoTitle
+        }
+      }
+    }
+  `)
+
   return (
     <Layout fullWidthContent={<div className={styles.background}></div>}>
       <Helmet>
         <base target="_blank" href="/" />
-        <title>Advertise | RUN BGD</title>
-        <meta
-          name="description"
-          content={`RUN BGD advertise page.`}
-        />
+        {
+          data.page.frontmatter.seoTitle ?
+          <title>{data.page.frontmatter.seoTitle}</title>
+          :
+          <title>Advertise | RUN BGD</title>
+        }
+        {data.page.frontmatter.seo ?
+          <meta
+            name="description"
+            content={data.page.frontmatter.seo}
+          />
+          :
+          <meta
+            name="description"
+            content={`RUN BGD advertise page.`}
+          />
+        }
       </Helmet>
       <main>
         <div className={styles.advertiseCard}>
