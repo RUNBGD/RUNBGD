c999bf86b3f2a004799f9593329da20fec116680
Milos Mladenovic <milos.mladenovic.work@gmail.com>
2020-12-18 10:24:48 +0100

    added vector icon for tabs

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -6,6 +6,7 @@ import Footer from '../Footer'
 import useSiteMetadata from '../SiteMetadata'
 
 import logo from '../../img/logo.jpeg'
+import logoVector from '../../img/logo-vector.svg'
 
 const Layout = ({ children, verticalSlider, fullWidthContent, fullWidth }) => {
   const { title, description } = useSiteMetadata()
@@ -22,17 +23,18 @@ const Layout = ({ children, verticalSlider, fullWidthContent, fullWidth }) => {
         <title>{title}</title>
         <meta name="description" content={description} />
 
-        <link rel="apple-touch-icon" sizes="180x180" href={logo} />
-        <link rel="icon" type="image/png" href={logo} sizes="32x32" />
-        <link rel="icon" type="image/png" href={logo} sizes="16x16" />
+        <link rel="apple-touch-icon" sizes="180x180" href={logoVector} />
+        <link rel="favicon" type="image/png" href={logoVector} />
+        <link rel="icon" type="image/png" href={logoVector} sizes="32x32" />
+        <link rel="icon" type="image/png" href={logoVector} sizes="16x16" />
 
-        <link rel="mask-icon" href={logo} color="#ff4400" />
+        <link rel="mask-icon" href={logoVector} color="#ff4400" />
         <meta name="theme-color" content="#fff" />
 
         <meta property="og:type" content="business.business" />
         <meta property="og:title" content={title} />
         <meta property="og:url" content="/" />
-        <meta property="og:image" content={logo} />
+        <meta property="og:image" content={logoVector} />
       </Helmet>
       {<Header />}
       {fullWidthContent}
